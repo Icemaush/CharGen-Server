d29fbbd1f8df63338b3b9b22f0f53b323b386107
Reece Pieri <reece@blueskylabs.com.au>
2021-07-14 17:12:37 +0800

    Update db

--- db/wow/knex.js
+++ db/wow/knex.js
@@ -1,7 +1,7 @@
 const knex = require('knex');
 const path = require('path');
 const databasePath = path.resolve('db/wow/wowgen.db');
-console.log(databasePath);
+
 // Connect to database
 const connectedKnex = knex({
     client: 'sqlite3',
